917da796f8b1fc0689368d08342b8181ca56aaa0
jurevito <jure.vito@gmail.com>
2023-10-17 13:12:39 +0200

    Test Google page parser

--- go.mod
+++ go.mod
@@ -7,4 +7,11 @@ require (
 	golang.org/x/net v0.17.0 // indirect
 )
 
-require github.com/andybalholm/cascadia v1.3.2 // indirect
+require github.com/stretchr/testify v1.8.4
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main_test.go
+++ main_test.go
@@ -6,7 +6,40 @@ import (
 	"testing"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/stretchr/testify/require"
 )
+
+func TestParse(t *testing.T) {
+	c, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatal("Could not read configuration file: ", err)
+	}
+
+	f, err := os.Open("pizza.html")
+	if err != nil {
+		log.Fatal("Could not read HTML file: ", err)
+	}
+	defer f.Close()
+
+	doc, err := goquery.NewDocumentFromReader(f)
+	if err != nil {
+		log.Fatal("File cannot be parsed as HTML: ", err)
+	}
+
+	res := parseDoc(doc, c)
+
+	// Check if number of parsed links is correct.
+	require.Equal(t, 98, len(res.Organic))
+	require.Equal(t, 28, len(res.Panel))
+	require.Equal(t, 3, len(res.Local))
+
+	expected := []int{5, 3, 6, 4}
+	require.Equal(t, len(expected), len(res.Caurasel))
+
+	for i := 0; i < len(res.Caurasel); i++ {
+		require.Equal(t, expected[i], len(res.Caurasel[i]))
+	}
+}
 
 func BenchmarkParse(b *testing.B) {
 
